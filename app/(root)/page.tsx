8ac44936808b51035b51821869b6a4260232c8f3
SomisCreations <89362410+samriddhighosh@users.noreply.github.com>
2025-12-04 20:08:39 -0600

    new commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { House, Users, ListChecks, Calendar, Search, Check, RefreshCcw, Eye, Flag, MessageCircle, CirclePlus } from 'lucide-react';
+import { Search, Check, RefreshCcw, Eye, Flag} from 'lucide-react';
 import DatePickerDateOfBirth from "@/components/DatePicker";
 import DataTableSimple from "@/components/DataTable";
 import BarChart02 from "@/components/BarChart";
@@ -15,7 +15,7 @@ export default function Home() {
       <div className="bg-gray-50 p-8 w-5/6 text-black flex flex-col gap-6">
         <div className="flex">
           <div>
-            <h3 className="text-[20px] font-semibold">Hello, Leonardo</h3>
+            <h2 className="text-[20px] font-semibold">Hello, Leonardo</h2>
             <p className="text-gray-700">What are you working on?</p>
           </div>
           <div className="w-[250px] h-10 rounded-4xl bg-gray-200 ml-30 flex gap-3 items-center p-3">
@@ -24,45 +24,45 @@ export default function Home() {
           </div>
         </div>
         <div className="flex gap-8">
-          <div className="border b p-3 rounded-2xl w-1/4 bg-white">
+          <div className="card-bg p-3 w-1/4">
             <div className="flex gap-2 items-center">
-              <div className="p-2 rounded-full bg-orange-200">
+              <div className="icon">
                 <Check size={16} />
               </div>
               <p className="text-[12px]">Daily Tasks</p>
             </div>
-            <h3 className="font-bold text-[24px] pt-4">2 Left</h3>
+            <h4 className="card-heading">2 Left</h4>
           </div>
-          <div className="border border-gray-300 p-3 rounded-2xl w-1/4 bg-white">
+          <div className="card-bg p-3 w-1/4">
             <div className="flex gap-2 items-center">
-              <div className="p-2 rounded-full bg-orange-200">
+              <div className="icon">
                 <RefreshCcw size={16} />
               </div>
               <p className="text-[12px]">Daily Habits</p>
             </div>
-            <h3 className="font-bold text-[22px] pt-2">2 Left</h3>
+            <h4 className="card-heading">2 Left</h4>
           </div>
-          <div className="border border-gray-300  p-3 rounded-2xl w-1/4 bg-white">
+          <div className="card-bg p-3 w-1/4">
             <div className="flex gap-2 items-center">
-              <div className="p-2 rounded-full bg-orange-200">
+              <div className="icon">
                 <Eye size={16} />
               </div>
               <p className="text-[12px]">Productivity Tips</p>
             </div>
-            <h3 className="font-bold text-[22px] pt-2">2 Left</h3>
+            <h4 className="card-heading">2 Left</h4>
           </div>
-          <div className="border border-gray-300 p-3 rounded-2xl w-1/4 bg-white">
+          <div className=" p-3 card-bg w-1/4 ">
             <div className="flex gap-2 items-center">
-              <div className="p-2 rounded-full bg-orange-200">
+              <div className="icon">
                 <Flag size={16} />
               </div>
               <p className="text-[12px]">Goal progress today</p>
             </div>
-            <h3 className="font-bold text-[22px] pt-2">2 Left</h3>
+            <h4 className="card-heading">2 Left</h4>
           </div>
         </div>
-        <div className="border-gray-300 border rounded-2xl bg-white">
-          <h3 className="font-semibold p-2 border-b border-gray-300">Daily Agenda</h3>
+        <div className="card-bg">
+          <h3>Daily Agenda</h3>
           <div>
             <div className="flex">
               <DatePickerDateOfBirth/>
@@ -71,17 +71,14 @@ export default function Home() {
           </div>
         </div>
         <div className="flex gap-6">
-          <div className="border-gray-300 border rounded-2xl bg-white w-3/4">
-            <h3 className="font-semibold p-3 border-b border-gray-300">Daily Agenda</h3>
+          <div className="card-bg w-3/4">
+            <h3>Daily Agenda</h3>
             <div>
                 <BarChart02/>
             </div>
           </div>
-          <div className="border-gray-300 border rounded-2xl bg-white w-1/4">
-            <div className="flex p-3 border-b">
-              <h3 className="font-semibold border-gray-300">People</h3>
-              
-            </div>
+          <div className="card-bg w-1/4">
+            <h3>People</h3>
             <div>
               <TableResponsive/>
             </div>
